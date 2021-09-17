56ae16045d87772ebf2b4b3899c257e0aa860444
Kip <88916382+UrbaneD0ge@users.noreply.github.com>
2021-09-16 23:34:40 -0400

    length validation

--- script.js
+++ script.js
@@ -13,5 +13,18 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// on button click start function, prompts
-onclick("click"): Window.alert("!");
+// on button click start function, prompt and store length
+function writePassword() {
+  var PWlength = prompt("Specify a password length between 8 - 128 charachters", "8-128");
+  console.log(PWlength);
+return;
+}
+
+if (( 8 <= PWlength) && (PWlength <= 128)) {
+  window.alert("That checks out...");
+} else {
+  PWlength = undefined;
+  writePassword();
+  console.log("Good enough")
+}
+// variable not scoped outside function, if statements not chained into initial function..
