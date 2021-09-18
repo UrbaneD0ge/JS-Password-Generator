8735649846de5727e7f88d5f41fff0a326208216
Kip <kipling.dunlap@gmail.com>
2021-09-17 21:58:59 -0400

    fix incorrect pw length

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
@@ -21,22 +20,23 @@ var num = undefined;
 
 // set of possible chars, copied from W3collective, modified by Kip
 const alpha = "abcdefghijklmnopqrstuvwxyz";
-const capitals ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const capitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
 const symbols = "!@#$%^&*_-+=";
 
 // on button click start function, prompt and store length
 function generatePassword() {
-  var PWlength = prompt("Specify a password length between 8 - 128 characters", "8-128");
-  
-// convert PWlength string to integer, log //
-  parseInt(PWlength);
+  var PWlength = prompt(
+    "Specify a password length between 8 - 128 characters",
+    "8-128"
+  );
+
+  // convert PWlength string to integer, log //
+  PWlength = parseInt(PWlength);
   console.log(PWlength);
 
-
-  // Validate PWlength 
-  if (( PWlength >= 8) && (PWlength <= 128)) {
-
+  // Validate PWlength
+  if (PWlength >= 8 && PWlength <= 128) {
     // Gather user specifications, store as booleans
     var spc = window.confirm("Include special characters?");
     console.log(spc);
@@ -47,24 +47,24 @@ function generatePassword() {
 
     // set password to blank, for loop to add characters //
     password = "";
-    for (var i = 0; i = PWlength; i++) {
+    console.log(typeof PWlength);
+
+    for (var i = 0; i < PWlength; i++) {
+    
       // model code from W3 demo
-      password += alpha.charAt(
-        Math.floor(Math.random() * PWlength)
-        );
-        // if spc = true, select from charset that includes 
-        if (spc = true) {
-          Math.random
-        }
-      console.log(password)
-      return password;
+      password += alpha.charAt(Math.floor(Math.random() * 26));
+      // if spc = true, select from charset that includes alpha + spc
+      // if (spc === true) {
+      //   Math.random();
+      // }
+      console.log(password.length);
+    
     }
 
   } else {
- 
     console.log("NOPE.");
     generatePassword();
     // error handling needed //
   }
-  return;
+  return password;
 }
